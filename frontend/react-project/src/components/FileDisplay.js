3541e9281c9eb5ce4dca5e8e0ba80a01f36041a9
Bryan <93906780+TheModerateCow@users.noreply.github.com>
2024-08-30 15:58:23 +0800

    modified:   frontend/react-project/src/App.js 	new file:   frontend/react-project/src/components/FileDisplay.js 	modified:   frontend/react-project/src/components/Header.js 	modified:   frontend/react-project/src/components/HomePage.js 	modified:   frontend/react-project/src/index.css

---
+++ frontend/react-project/src/components/FileDisplay.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function FileDisplay(props) {
+	const { handleAudioReset, file, audioStream } = props;
+
+	return (
+		<main className="flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20">
+			<h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
+				Your <span className="text-blue-400 bold">File</span>
+			</h1>
+			<div className="flex items-center gap-2">
+				<h3 className="font-semibold">Name</h3>
+				<p>{file.name}</p>
+			</div>
+		</main>
+	);
+}
--- frontend/react-project/src/components/HomePage.js
+++ frontend/react-project/src/components/HomePage.js
@@ -1,11 +1,38 @@
 import React from "react";
 
-export default function HomePage() {
+export default function HomePage(props) {
+	const { setAudioStream, setFile } = props;
+
 	return (
-		<main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center">
-			<h1 className="font-semibold text-5xl sm:text-6xl">
+		<main className="flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20">
+			<h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
 				Free <span className="text-blue-400 bold">Scribe</span>
 			</h1>
+			<h3 className="font-medium md:text-lg">
+				Record <span className="text-blue-400">&rarr;</span> Transcribe{" "}
+				<span className="text-blue-400">&rarr;</span> Translate
+			</h3>
+			<button className="flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4">
+				<p className="text-blue-400">Record</p>
+				<i className="fa-solid fa-microphone"></i>
+			</button>
+			<p className="text-base">
+				Or{" "}
+				<label className="text-blue-400 cursor-pointer hover:text-blue-600 duration-200">
+					upload{" "}
+					<input
+						onChange={(e) => {
+							const tempFile = e.target.files[0];
+							setFile(tempFile);
+						}}
+						className="hidden"
+						type="file"
+						accept=".mp3,.wave,.pdf"
+					></input>
+				</label>{" "}
+				a mp3 file
+			</p>
+			<p className="italic text-slate-500">Free now free forever</p>
 		</main>
 	);
 }
