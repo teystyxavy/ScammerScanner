04ca287d983daf6a3772bd6d1b1eba2840e08a22
Daniel <ggsan.com@gmail.com>
2024-08-31 06:09:46 +0800

    integrated code into __init__.py for app, now can accept POST requests (but need frontend to fix sending)

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,47 @@
 import os
 import sqlite3
-from flask import Flask
+from flask import Flask, jsonify, request
+from io import BytesIO
+from PIL import Image
+from app.services import *
 from .views import views
 from .db_setup import create_database, insert_dummy_data
 
-
+app = Flask(__name__)
 DB_NAME = "ScamDetectorDB.db"
 
-def create_app():
-    app = Flask(__name__)
+# route for image uploads
+@app.route('/upload', methods=['POST'], strict_slashes=False)
+def check_scam():
+    if 'files[]' not in request.files:
+        res = jsonify({
+            'message' : "Nothing transmitted",
+            'status' : "failed"
+        })
+        res.status_code = 400
+        return res
+    
+    file = request.files.getlist('files[]')[0]
+    image = Image.open(file).convert('L')
 
+    parse_text = ParseTextService()
+
+    text = parse_text.extract_text(image)
+
+    first_check = ScamFirstCheckService(text=text)
+    malicious = first_check.check_scam()
+
+    res = jsonify({
+        'content' : text,
+        'malicious' : malicious,
+        'message' : "successfully transmitted",
+        'status' : "success"
+    })
+
+    return res
+
+
+def create_app():
     # Create the SQLite database and tables if they don't exist
     if not os.path.exists(DB_NAME):
         create_database()
