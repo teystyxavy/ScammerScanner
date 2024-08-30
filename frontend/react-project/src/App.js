59d0b0849fcb354c6e9b117c161079f918b35af3
Bryan <93906780+TheModerateCow@users.noreply.github.com>
2024-08-30 14:25:07 +0800

    modified:   .gitignore 	new file:   frontend/react-project/README.md 	new file:   frontend/react-project/package-lock.json 	new file:   frontend/react-project/package.json 	new file:   frontend/react-project/public/favicon.ico 	new file:   frontend/react-project/public/index.html 	new file:   frontend/react-project/public/logo192.png 	new file:   frontend/react-project/public/logo512.png 	new file:   frontend/react-project/public/manifest.json 	new file:   frontend/react-project/public/robots.txt 	new file:   frontend/react-project/src/App.css 	new file:   frontend/react-project/src/App.js 	new file:   frontend/react-project/src/App.test.js 	new file:   frontend/react-project/src/components/Header.js 	new file:   frontend/react-project/src/components/HomePage.js 	new file:   frontend/react-project/src/index.css 	new file:   frontend/react-project/src/index.js 	new file:   frontend/react-project/src/logo.svg 	new file:   frontend/react-project/src/reportWebVitals.js 	new file:   frontend/react-project/src/setupTests.js 	new file:   frontend/react-project/tailwind.config.js

---
+++ frontend/react-project/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { useState } from "react";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
+
+function App() {
+	const [count, setCount] = useState(0);
+	return (
+		<div className="flex flex-col max-w-[1000px] mx-auto w-full">
+			<section className="min-h-screen flex flex-col">
+				<Header />
+				<HomePage />
+			</section>
+			<h1 className="text-green-400">hello</h1>
+			<footer></footer>
+		</div>
+	);
+}
+
+export default App;
