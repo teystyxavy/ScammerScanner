f971209a4733c3e533ffac1efefa0986d10ee973
Xavier Tey <xavier.tey.2023@scis.smu.edu.sg>
2024-08-30 18:40:17 +0800

    edited models.py to include User and Post class

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,23 @@
 from flask import Flask
-from flask_migrate import Migrate
+##from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from .views import views
 from .auth import auth
+from os import path
 
-## db = SQLAlchemy()
-migrate = Migrate()
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+##migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
     # config settings
     app.config.from_object('config.Config')
-
-    # Initialize extensions
-    ## db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME};'   # Initialize extensions
+    db.init_app(app)
+    
     ## migrate.init_app(app, db)
 
     # Register blueprints
@@ -24,4 +27,11 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Post
+
     return app
+
+def create_database(app):
+    if not path.exists('app/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 ## for database models
+from . import db
+from sqlalchemy.sql import func
+from flask_login import UserMixin
 
-## from . import db
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(150))
+    posts = db.relationship('Post')
+    
+class Post(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    date_posted = db.Column(db.DateTime(timezone=True), primary_key=True, default=func.now()) #retrieves current time and stores it in the DateTime field
+    content = db.Column(db.String(10000))
+
+
+
+
